401d9afbbffc9d5b9a7abbff0f4f400a71cdf175
ext.amit.r <ext.amit.r@grabtaxi.com>
2024-02-13 12:11:54 +0530

    Rearranged test case to avoid test case failure

--- server/handlers/features_test.go
+++ server/handlers/features_test.go
@@ -84,21 +84,21 @@ func TestRemoveUser(t *testing.T) {
 
 func TestViewSeats(t *testing.T) {
 
-	t.Run("happy path", func(t *testing.T) {
-		mockViewSeat := &bt.SectionRequest{
-			Section: "A",
-		}
-		mockserver.tickets[mockViewSeat.Section] = &bt.TicketResponse{}
-		resp, _ := mockserver.ViewSeats(context.Background(), mockViewSeat)
-		assert.NotNil(t, resp)
-	})
-
 	t.Run("sad path - user not found error", func(t *testing.T) {
 		mockViewSeat := &bt.SectionRequest{
 			Section: "Z",
 		}
 		_, err := mockserver.ViewSeats(context.Background(), mockViewSeat)
 		assert.EqualError(t, err, "no seats have been booked yet")
+	})
+
+	t.Run("happy path", func(t *testing.T) {
+		mockViewSeat := &bt.SectionRequest{
+			Section: "A",
+		}
+		mockserver.tickets[mockViewSeat.Section] = &bt.TicketResponse{From: "Italy", To: "France", UserDetails: &bt.User{EmailAddress: "Amit.r@gmail.com"}}
+		resp, _ := mockserver.ViewSeats(context.Background(), mockViewSeat)
+		assert.NotNil(t, resp)
 	})
 
 	t.Run("sad path - section not there", func(t *testing.T) {
